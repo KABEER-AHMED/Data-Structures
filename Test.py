5e2e9f6fae44fc4ed60367275dafed937cf98161
KABEER-AHMED <kabeerkd786@gmail.com>
2025-05-23 13:08:34 +0300

    updated the linked list file due to silly mistake, forgot the 'itr=itr.next' part

--- Test.py
+++ Test.py
@@ -1,37 +1,73 @@
-class Deque():
-    def __init__ (self):
-        self.deque = []
-    def addFront(self,item):
-        self.deque.insert(0,item)
-    def addRear(self,item):
-        self.deque.append(item)
-    def removeFront(self):
-        return self.deque.pop(0)
-    def removeRear(self):
-        return self.deque.pop()
-    def isEmpty(self):
-        return not bool(self.deque)
-    def Size(self):
-        return len(self.deque)
+# Creating the node class
+class Node:
+    def __init__(self,data):
+        self._data = data
+        self._next = None
+    def get_data(self):
+        return self._data
+    def set_data(self,data):
+        self._data = data
     
-# creating the logic for the program
-def Palindrome_checker(word):
-    deque = Deque()
-    for letter in word:
-        deque.addRear(letter)
-    same = True
-    while deque.Size()>1 and same:
-        first_letter = deque.removeFront()
-        last_letter = deque.removeRear()
-        if first_letter != last_letter :
-            same = False
-    return same
+    data = property(get_data,set_data)
+    
+    def get_next(self):
+        return self._next
+    def set_next(self,node_next):
+        self._next = node_next
+    
+    next = property(get_next,set_next)
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def add_node(self,item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+    def print(self):
+        llstr =''
+        iterator = self.head
+        if iterator == None:
+            print("Linked list is empty")
+            return None
+        while iterator!=None:
+            llstr += str(iterator.data) + "-->"
+            iterator = iterator.next
+        print(llstr)
+    
+    
+    def search(self,value):
+        itr = self.head
+        index = 0
+        while itr:
+            if itr.data == value:
+                return index
+            itr = itr.next
+            index+=1
+    
+    def remove(self,item):
+        count = 0
+        index = self.search(item)
+        if index is None:
+            print("The item was not found")
+            return
+        if index == 0:  
+            self.head = self.head.next
+            return
+        itr = self.head
+        while itr:
+            if count == index-1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count+=1
 
-# Main program
-print("Hello there !, i see you want to check whether you word is a palindrome or not! ")
-word = input("enter the word please: ")
-result = Palindrome_checker(word)
-if result:
-    print(f"Congratulations! , the word {word} is a palindrome")
-else:
-    print(f"Sorry, the word {word} is not a palindrome")
+ll = LinkedList()
+ll.add_node(45)
+ll.add_node(50)
+ll.add_node(55)
+ll.print()
+ll.add_node(66)
+ll.print()
+ll.remove(50)
+ll.print()
