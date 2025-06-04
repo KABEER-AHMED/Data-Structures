a229d9f5092f9e6584fcf77442762222732b20a6
KABEER-AHMED <kabeerkd786@gmail.com>
2025-06-04 08:54:49 +0300

    committing the left over data

--- Test.py
+++ Test.py
@@ -1,73 +1,25 @@
-# Creating the node class
-class Node:
-    def __init__(self,data):
-        self._data = data
-        self._next = None
-    def get_data(self):
-        return self._data
-    def set_data(self,data):
-        self._data = data
-    
-    data = property(get_data,set_data)
-    
-    def get_next(self):
-        return self._next
-    def set_next(self,node_next):
-        self._next = node_next
-    
-    next = property(get_next,set_next)
-    
-class LinkedList:
-    def __init__(self):
-        self.head = None
-    def add_node(self,item):
-        temp = Node(item)
-        temp.set_next(self.head)
-        self.head = temp
-    def print(self):
-        llstr =''
-        iterator = self.head
-        if iterator == None:
-            print("Linked list is empty")
-            return None
-        while iterator!=None:
-            llstr += str(iterator.data) + "-->"
-            iterator = iterator.next
-        print(llstr)
-    
-    
-    def search(self,value):
-        itr = self.head
-        index = 0
-        while itr:
-            if itr.data == value:
-                return index
-            itr = itr.next
-            index+=1
-    
-    def remove(self,item):
-        count = 0
-        index = self.search(item)
-        if index is None:
-            print("The item was not found")
-            return
-        if index == 0:  
-            self.head = self.head.next
-            return
-        itr = self.head
-        while itr:
-            if count == index-1:
-                itr.next = itr.next.next
-                break
-            itr = itr.next
-            count+=1
+import turtle
 
-ll = LinkedList()
-ll.add_node(45)
-ll.add_node(50)
-ll.add_node(55)
-ll.print()
-ll.add_node(66)
-ll.print()
-ll.remove(50)
-ll.print()
+
+def tree(branch_len, t):
+    if branch_len > 5:
+        t.forward(branch_len)
+        t.right(20)
+        tree(branch_len - 15, t)
+        t.left(40)
+        tree(branch_len - 15, t)
+        t.right(20)
+        t.backward(branch_len)
+
+def main():
+    t = turtle.Turtle()
+    my_win = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(200)
+    t.down()
+    t.color("green")
+    tree(100, t)
+    my_win.exitonclick()
+
+main()
