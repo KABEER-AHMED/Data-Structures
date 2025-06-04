11f2c20d1e3392c79be6c44568c435b40fea3934
KABEER-AHMED <kabeerkd786@gmail.com>
2025-06-04 09:21:33 +0300

    finished the implementation of binary search

---
+++ Search_Sort.py
@@ -0,0 +1,17 @@
+def binary_search(num_list,item):
+    left_index = 0
+    right_index = len(num_list) - 1
+    while left_index<= right_index:
+        middle_index = (left_index+right_index ) //2
+        middle_number = num_list[middle_index]
+        if middle_number == item:
+            print("found the item at position {}".format(middle_index))
+            return middle_index
+        elif item < middle_number:
+            right_index = middle_index - 1
+        else:
+            left_index = middle_index+1
+    return -1
+
+lst = [1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,19,22,23,34,45,56,67,78]
+binary_search[lst,23]
